a9be95592058abd8f3b998ba9e18f6c0c8fe3a13
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-06-10 15:06:45 +1000

    Made you  able to connect to the robo car via cable!
    Bluetooth coming soon!

---
+++ OtherCode/robo-carOLD.ino
@@ -0,0 +1,236 @@
+/****Introduce IR Remote Control header file***/
+#include <IRremote.h>
+
+int RECV_PIN = A4;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+/****JM1Motor controls the pins***/
+#define PWM1  3
+#define M1    2
+/****JM4Motor controls the pins***/
+#define PWM4  11
+#define M4    8
+
+/****JM2Motor controls the pins***/
+#define PWM2  5
+#define M2    4
+/****JM3Motor controls the pins***/
+#define PWM3  6
+#define M3    7
+
+/******** Speed compensation ***********/
+#define compensation  28
+
+#define speed1   100          //Righr front motor speed
+#define speed4   100          //Righr rear motor speed
+
+#define speed2   speed1       //Left front motor speed
+#define speed3   speed4       //Left rear motor speed
+
+/*******Ultrasonic Sensor interface*****/
+#define EchoPin  13  //ECHO to D13
+#define TrigPin  12  //TRIG to D12
+
+void setup()
+{
+  Serial.begin(9600); //Set baud rate to 9600
+  pinMode(EchoPin, INPUT);    //The ECHO pin is set to input mode
+  pinMode(TrigPin, OUTPUT);   //The TRIG pin is set to output mode
+  /****All motor control pins are set to output mode***/
+  pinMode(M1, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+  pinMode(M3, OUTPUT);
+  pinMode(PWM3, OUTPUT);
+  pinMode(M4, OUTPUT);
+  pinMode(PWM4, OUTPUT);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  float distance = Get_Distance();  //Get the distance and save in the distance variable  
+  Serial.print("distance:");
+  Serial.print(distance);
+  Serial.println(" cm");
+  if (irrecv.decode(&results)) {
+    switch (results.value)
+    {
+      case 0xFF629D: Advance(); break;//
+      case 0xFFA857: Back();    break;//
+      case 0xFF22DD: Turn_Left();  break;//
+      case 0xFFC23D: Turn_Right(); break;//
+      case 0xFF02FD: Stop();    break;//OK
+      case 0xFF30CF: L_Move(); break;//4
+      case 0xFF7A85: R_Move(); break;//6
+      case 0xFF6897: LU_Move(); break;//1
+      case 0xFF10EF: LD_Move(); break;//7
+      case 0xFFB04F: RU_Move(); break;//3
+      case 0xFF5AA5: RD_Move(); break;//9
+      default: break;
+    }
+    irrecv.resume(); // Receive the next value
+  }
+
+}
+
+float Get_Distance(void) {    //Ultrasonic detects the distance
+  float dis;
+  digitalWrite(TrigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH); //Give the TRIG a high level of at least 10 µ s to trigger
+  delayMicroseconds(10);
+  digitalWrite(TrigPin, LOW);
+  dis = pulseIn(EchoPin, HIGH) /58.2;  //Work out the distance
+  delay(50);
+  return dis;
+}
+
+/**********The car advance***********/
+void Advance(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, speed1);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, speed4);
+
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, speed2+compensation);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, speed3+compensation);
+}
+
+/**********The car moves back***********/
+void Back(void) {
+  digitalWrite(M1, HIGH);
+  analogWrite(PWM1, 255 - speed1);
+  digitalWrite(M4, HIGH);
+  analogWrite(PWM4, 255 - speed4);
+
+  digitalWrite(M2, HIGH);
+  analogWrite(PWM2, 255 - speed2);
+  digitalWrite(M3, HIGH);
+  analogWrite(PWM3, 255 - speed3);
+}
+
+/**********The car rotates left***********/
+void Turn_Left(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, speed1);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, speed4);
+
+  digitalWrite(M2, HIGH);
+  analogWrite(PWM2, 255 - speed2);
+  digitalWrite(M3, HIGH);
+  analogWrite(PWM3, 255 - speed3);
+}
+
+/**********The car rotates right***********/
+void Turn_Right(void) {
+  digitalWrite(M1, HIGH);
+  analogWrite(PWM1, 255 - speed1);
+  digitalWrite(M4, HIGH);
+  analogWrite(PWM4, 255 - speed4);
+
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, speed2+compensation);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, speed3+compensation);
+}
+
+/**********The car stops***********/
+void Stop(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, 0);
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, 0);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, 0);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, 0);
+}
+
+/**********The car moves on the left***********/
+void L_Move(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, speed1);
+  digitalWrite(M4, HIGH);
+  analogWrite(PWM4, 255 - speed4);
+
+  digitalWrite(M2, HIGH);
+  analogWrite(PWM2, 255 - speed2);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, speed3+compensation);
+}
+
+
+/**********The car moves on the right***********/
+void R_Move(void) {
+  digitalWrite(M1, HIGH);
+  analogWrite(PWM1, 255 - speed1);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, speed4);
+
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, speed2+compensation);
+  digitalWrite(M3, HIGH);
+  analogWrite(PWM3, 255 - speed3);
+}
+
+/**********The car moves on the left front***********/
+void LU_Move(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, speed1);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, 0);
+
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, 0);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, speed3+compensation);
+}
+
+
+/**********The car moves on the left rear***********/
+void LD_Move(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, 0);
+  digitalWrite(M4, HIGH);
+  analogWrite(PWM4, 255 - speed4);
+
+  digitalWrite(M2, HIGH);
+  analogWrite(PWM2, 255 - speed2);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, 0);
+}
+
+
+/**********The car moves on the right front***********/
+void RU_Move(void) {
+  digitalWrite(M1, LOW);
+  analogWrite(PWM1, 0);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, speed4);
+
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, speed2+compensation);
+  digitalWrite(M3, LOW);
+  analogWrite(PWM3, 0);
+}
+
+
+/**********The car moves on the right rear***********/
+void RD_Move(void) {
+  digitalWrite(M1, HIGH);
+  analogWrite(PWM1, 255 - speed1);
+  digitalWrite(M4, LOW);
+  analogWrite(PWM4, 0);
+
+  digitalWrite(M2, LOW);
+  analogWrite(PWM2, 0);
+  digitalWrite(M3, HIGH);
+  analogWrite(PWM3, 255 - speed3);
+}
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import serial
 
-arduino = serial.Serial(port='COM3',  baudrate=9600, timeout=.1)
+arduino = serial.Serial(port='/dev/ttyUSB0',  baudrate=9600, timeout=.1)
 while True:
     data = arduino.readline()
     clean = data.strip(b'\xff\xdf')
