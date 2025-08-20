fd3bc90af41c05f577b9d3dbdbddfea297cd7033
Tsunami014 <tsunami014@duck.com>
2025-08-20 10:09:10 +1000

    Got the banana back and now it's pretty great!

--- readCam.py
+++ readCam.py
@@ -2,8 +2,50 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 import math
+from collections import defaultdict
 
-# TODO: Fix flickering
+
+DAMPENING = 0.0003  # The dampening from the movement predictions.
+# TODO: Intorpelate between values when going off predictions
+
+cameraMatrix, distCoeffs = None, None
+# The difference every frame
+x, y, z = 0, 0, 0
+p, r, yw = 0, 0, 0
+class Tag:
+    Timeout = 30
+    def __init__(self):
+        self.called = False
+        self.untilDeath = self.Timeout
+        self.pos = (0, 0, 0)
+        self.rot = (0, 0, 0)
+        self.sze = 0
+
+    def __call__(self, found, *args):
+        if self.called:
+            return
+        self.called = True
+        if found:
+            self.pos, self.rot, self.sze = args[:3]
+            self.untilDeath = self.Timeout
+        else:
+            self.untilDeath -= 1
+            if self.untilDeath <= 0:
+                idx = list(tagsL.values()).index(self)
+                id = list(tagsL.keys())[idx]
+                tagsL.pop(id)
+                return False
+            self.pos = (self.pos[0]+x*DAMPENING, self.pos[1]+y*DAMPENING, self.pos[2]+z*DAMPENING)
+            self.rot = (self.rot[0]+p*DAMPENING, self.rot[1]+r*DAMPENING, self.rot[2]+yw*DAMPENING)
+
+    def draw(self, frame):
+        ps, _ = cv2.projectPoints(np.array([[[0, 0, 0]]], dtype=np.float32), np.array(self.rot, dtype=np.float32), np.array(self.pos, dtype=np.float32), cameraMatrix, distCoeffs)
+        p = ps[0][0]
+        nbanana = cv2.resize(banana, (self.sze, self.sze))
+        h, w, _ = nbanana.shape
+        overlay_img(frame, nbanana, int(p[0]-w/2), int(p[1]-h/4*3))
+
+tagsL = defaultdict(Tag)
 
 marker_length = 0.0036  # Marker size in meters
 
@@ -53,8 +95,6 @@ aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
 arucoParameters = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, arucoParameters)
 prevFrame = None
-x, y, z = 0, 0, 0
-p, r, yw = 0, 0, 0
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -68,35 +108,36 @@ while True:
                              [0, 0, 1]], dtype=np.float32)
     distCoeffs = np.zeros(5)  # Assume no distortion
 
+    # Unset called in every tag
+    for t in tagsL:
+        tagsL[t].called = False
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
+    # Loop over found tags
     if ids is not None:
         rvecs, tvecs = estimate_pose(corners, marker_length, cameraMatrix, distCoeffs)
 
-        for i in range(len(ids)):
-            # Draw axis for each marker
-            frame = cv2.drawFrameAxes(frame, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], marker_length*0.5)
+        for i, id in enumerate(ids):
+            id = id[0]
+            cns = corners[i]
+            x1, y1, z1 = tvecs[i]
+            x1, y1, z1 = x1[0], y1[0], z1[0]
+            p1, r1, yw1 = rvecs[i]  # Pitch Roll Yaw
+            p1, r1, yw1 = p1[0], r1[0], yw1[0]
 
-            """# Print 3D position
-            x, y, z = tvecs[i]
-            x, y, z = x[0], y[0], z[0]
-            p, r, yw = rvecs[i]  # Pitch Roll Yaw
-            p, r, yw = p[0], r[0], yw[0]
-            print(f"Marker ID {ids[i][0]} position: x={x:.2f}, y={y:.2f}, z={z:.2f}, pitch={p:.2f}, roll={r:.2f}, yaw={yw:.2f}")"""
-
-        """for idx in range(len(ids)):
-            id, cns = ids[idx], corners[idx]
-            cv2.fillPoly(frame, cns.astype(np.int32), (250, 250, 250))
             centre = np.array((
                 sum(i[0] for i in cns[0])/len(cns[0]),
                 sum(i[1] for i in cns[0])/len(cns[0])
             ), dtype=np.int64)
             std = (find_std([i[0] for i in cns[0]]), find_std([i[1] for i in cns[0]]))
             mstd = max(std)*2
-            nbanana = cv2.resize(banana, (mstd, mstd))
-            h, w, _ = nbanana.shape
-            overlay_img(frame, nbanana, centre[0]-w//2, int(centre[1]-h/4*3))"""
-    elif prevFrame is not None:
+
+            tagsL[id](True, (x1, y1, z1), (p1, r1, yw1), mstd)
+
+    x, y, z = 0, 0, 0
+    p, r, yw = 0, 0, 0
+    if prevFrame is not None:
         orb = cv2.ORB_create()
         kp1, des1 = orb.detectAndCompute(frame, None)
         kp2, des2 = orb.detectAndCompute(prevFrame, None)
@@ -115,14 +156,15 @@ while True:
             except Exception:
                 done = False
             if done:
-                dx, dy, dz = t
-                dx, dy, dz = dx[0], dy[0], dz[0]
-                dp, dr, dyw = R  # Pitch Roll Yaw
-                dp, dr, dyw = dp[0], dr[0], dyw[0]
+                x, y, z = t
+                x, y, z = x[0], y[0], z[0]
+                p, r, yw = R  # Pitch Roll Yaw
+                p, r, yw = p[0], r[0], yw[0]
                 p = p-1
-                x, y, z = x + dx, y + dy, z + dz
-                p, r, yw = p + dp, r + dr, y + dyw
-                print(f"Movement position:\n    x={fmt(x)}, y={fmt(y)}, z={fmt(z)},\n    pitch={fmt(p)}, roll={fmt(r)}, yaw={fmt(yw)}")
+
+    for t in tagsL.copy():
+        if tagsL[t](False) is not False:
+            tagsL[t].draw(frame)
 
     prevFrame = frame
     cv2.imshow('Display', frame)
